4a48beb363c2897775f1251fc9ecc6d362e6663d
Gaddam Bheemesh <bheemeshgaddam008@gmail.com>
2026-07-19 14:36:28 +0530

    Add files via upload

--- api/index.py
+++ api/index.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-# Add parent directory to path so python can find the app modules
+# Add the parent directory to the path so python can find perfect_indian_app
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from perfect_indian_app import app
